49cae4658e61b65881a4336abb50cb4abd3acded
Tusharhow <tusharsoftdev@gmail.com>
2021-02-02 23:10:34 -0800

    Day 2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = 'codepur';
+    return Scaffold(
+      appBar: AppBar(title: Text('30 Days Flutter'),),
+      body: Center(
+        child: Text('Welcome to $days days of flutter by $name')),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_30days/homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Material(
-      child: Center(child: Text('This is the flutter')),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
   }
 }
