3cf869fa2c5fc4527fbefe8fc2620b17a0c0e46b
Tusharhow <tusharsoftdev@gmail.com>
2021-02-09 21:29:08 -0800

    Day 9 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_30days/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,7 +12,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text('30 Days Flutter'),),
       body: Center(
         child: Text('Welcome to $days days of flutter by $name')),
-        drawer: Drawer(),
+        drawer: MyDrawer(),
     );
   }
 }
