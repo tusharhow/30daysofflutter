5ecbe09273fd379a0ea07be2ed5d81c5db8984b3
tusharhow <tusharsoftdev@gmail.com>
2021-02-18 09:57:26 +0600

    Day 15 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -7,18 +7,28 @@ class Item {
   final String color;
 
   Item({this.id, this.name, this.desc, this.image, this.price, this.color});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      price: map["price"],
+      desc: map["desc"],
+      image: map["image"],
+      color: map["color"],
+    );
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "image": image,
+        "color": color,
+      };
 }
 
 class CatalogModel {
-  static final items = [
-    Item(
-      id: 1,
-      color: "#33505a",
-      desc: "Appple iPhone 12 Generation",
-      image:
-          "http://www.pngall.com/wp-content/uploads/5/Apple-iPhone-12-PNG-File.png",
-      name: "iPhone 12 Pro",
-      price: 999,
-    )
-  ];
+  static List <Item> items;
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -21,31 +21,40 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString('assets/files/catalog.json');
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData['products'];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     // final int days = 30;
     // final String name = 'codepur';
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
+        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)? ListView.builder(
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummyList[index],
+              item: CatalogModel.items[index],
             );
           },
-        ),
+        ): Center(
+          child: CircularProgressIndicator(),
+        )
+        
+
       ),
       drawer: MyDrawer(),
     );
